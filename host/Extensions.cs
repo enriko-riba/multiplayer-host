62b9370c0266b641e0749706007b095b37f0bff6
Enriko Riba <enriko.riba@gmail.com>
2020-11-30 18:06:26 +0100

    cleanup

--- host/Extensions.cs
+++ host/Extensions.cs
@@ -3,8 +3,17 @@ using Microsoft.Extensions.Logging;
 
 namespace MultiplayerHost
 {
+    /// <summary>
+    /// Generic extensions.
+    /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Adds the caller method name to log properties.
+        /// </summary>
+        /// <param name="logger"></param>
+        /// <param name="method"></param>
+        /// <returns></returns>
         public static ILogger WithMethodName(this ILogger logger, [CallerMemberName] string method = null)
         {
             var name = (method?.StartsWith('.') ?? false) ? method : '.' + method;
