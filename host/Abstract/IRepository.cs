99a169514aaebbb9d47303cd21dac1c2baedcf1c
Enriko Riba <enriko.riba@gmail.com>
2020-12-02 11:45:47 +0100

    fixes comments spelling errors

--- host/Abstract/IRepository.cs
+++ host/Abstract/IRepository.cs
@@ -11,28 +11,28 @@
     public interface IRepository
     {
         /// <summary>
-        /// Returns a collection off all users from the persistance layer.
+        /// Returns a collection off all users from the persistence layer.
         /// Note: the implementations can and should return a filtered collection of only users eligible for playing the game e.g. without deactivated, banned or any other inactive user types.
         /// </summary>
         /// <returns></returns>
         Task<IEnumerable<User>> GetUsers();
 
         /// <summary>
-        /// Returns a user from the persistance layer.
+        /// Returns a user from the persistence layer.
         /// </summary>
         /// <param name="userId"></param>
         /// <returns></returns>
         Task<User> GetUserAsync(int userId);
 
         /// <summary>
-        /// Saves the user to the persistance layer.
+        /// Saves the user to the persistence layer.
         /// </summary>
         /// <param name="user"></param>
         /// <returns></returns>
         Task SaveUserAsync(User user);
 
         /// <summary>
-        /// Permanently deletes the user from the persistance layer.
+        /// Permanently deletes the user from the persistence layer.
         /// </summary>
         /// <param name="user"></param>
         /// <returns></returns>
